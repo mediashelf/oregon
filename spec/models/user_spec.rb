3a61e6d53dd62aa41ab5f23b1208fe63f81a2a81
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-26 12:48:58 -0600

    Added a spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(email: 'user1@example.com', password: 'foo')
+  end
+  it "should validate that the password is more than 6 characters" do
+    subject.should_not be_valid
+    subject.errors[:password].should == ["is too short (minimum is 6 characters)"]
+  end
 end
