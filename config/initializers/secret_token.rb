fbe155a6b6ca2ebecb8ccaf4b43429cede0a8664
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-26 11:53:39 -0600

    New rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OregonDigital::Application.config.secret_token = 'de846e04b6febac21a7d6fc5678e0eed08e1816685a623706cc2acbb4e10da18c50a9a0682789bae0fd61e58842a8a5b3235e88583ccabea6fb50d57c85a6604'
