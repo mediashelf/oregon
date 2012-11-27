a8d4b3018c23b72a1f2674d6cfa3f99f89f32200
Jeremy Echols <jechols@uoregon.edu>
2012-11-27 10:39:51 -0800

    HYUO-6: Use ruby 1.9 hash syntax

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,8 +3,8 @@ class Image < ActiveFedora::Base
   has_metadata :name => "descMetadata", :type => OregonRDFDatastream
 
   # see https://github.com/mediashelf/sufia/blob/master/app/models/datastreams/file_content_datastream.rb
-  has_file_datastream :name => "content"
-  has_file_datastream :name => "thumbnail"
+  has_file_datastream name: "content"
+  has_file_datastream name: "thumbnail"
 
   delegate_to :descMetadata, [:hasFormat, :type, :spatial, :created, :description, :rights, :title, :subject, :identifier, :modified, :date]
 end
