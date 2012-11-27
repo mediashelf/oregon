47eb3eabbe17221d2e7d7e39029d4c9844449f4c
Thomas Johnson <thomas.johnson@oregonstate.edu>
2012-11-26 20:56:34 -0800

    delegate PREMIS fields

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ require 'datastreams/oregon_rdf_datastream'
 class Image < ActiveFedora::Base
   has_metadata :name => "descMetadata", :type => OregonRDFDatastream
 
-  delegate_to :descMetadata, [:hasFormat, :type, :spatial, :created, :description, :rights, :title, :subject, :identifier, :modified, :date]
+  delegate_to :descMetadata, [:creator, :contributor, :abstract, :description, :type, :spatial, :created, :rights, :title, :subject, :identifier, :modified, :date, :hasFormat, :format, :preservation, :fixity]
 end
