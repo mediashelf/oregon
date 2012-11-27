54e9161f2a89a959876515a1bc403d964b2780d8
Jeremy Echols <jechols@uoregon.edu>
2012-11-27 13:31:43 -0800

    HYUO-11: Unfix hydra access duped stuff

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -10,15 +10,12 @@ class CatalogController < ApplicationController
   # These before_filters apply the hydra access controls
   before_filter :enforce_access_controls
   before_filter :enforce_viewing_context_for_show_requests, :only=>:show
+
   # This applies appropriate access controls to all solr queries
   CatalogController.solr_search_params_logic << :add_access_controls_to_solr_params
+
   # This filters out objects that you want to exclude from search results, like FileAssets
   CatalogController.solr_search_params_logic << :exclude_unwanted_models
-  include Hydra::AccessControlsEnforcement
-
-  before_filter :enforce_access_controls
-
-  CatalogController.solr_search_params_logic << :add_access_controls_to_solr_params
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-# Connects this user object to Hydra behaviors. 
- include Hydra::User
-  include Blacklight::User
+  # Connects this user object to Hydra behaviors. 
   include Hydra::User
+  include Blacklight::User
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
