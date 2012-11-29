ee576c704833866248e0ae3bee9f35e1ecdb05f4
Jeremy Echols <jechols@uoregon.edu>
2012-11-29 11:53:09 -0800

    HYUO-17: Test ip-based group

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,20 @@ describe User do
     subject.should_not be_valid
     subject.errors[:password].should == ["is too short (minimum is 6 characters)"]
   end
+
+  describe "#groups" do
+    context "(for a user without a special IP address)" do
+      it "should include 'library'" do
+        subject.current_sign_in_ip = "128.223.223.89"
+        subject.groups.should include("library")
+      end
+    end
+
+    context "(for a user without a special IP address)" do
+      it "should not include 'library'" do
+        subject.current_sign_in_ip = "28.223.223.89"
+        subject.groups.should_not include("library")
+      end
+    end
+  end
 end
